635f4c29245b3dc1c26d9381abd7d2ab45e4c801
shiva chapagain <shibuchapagain12@gmail.com>
2022-08-13 16:42:07 +0545

    8-13-2022

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
+  useState();
+  let title = props.title;
+
+  const clickHandler = () => {
+    // console.log("CLICKED!!!");
+    title = "UPDATED";
+    console.log(title);
+  };
   return (
-    <div class="expense-item">
+    <div className="expense-item">
       <div>
         <ExpenseDate date={props.date}></ExpenseDate>
       </div>
-      <div class="expense-item__description">
-        <h2> {props.title} </h2>
-        <div class="expense-item__price ">${props.amount}</div>
+      <div className="expense-item__description">
+        <h2> {title} </h2>
+        <div className="expense-item__price ">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>CHANGE ITEM</button>
     </div>
   );
 }
